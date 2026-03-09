da42c2ea7c00c7310b617e975b958d620d91cc1c
ThePrismSystem <10910959+ThePrismSystem@users.noreply.github.com>
2026-03-09 07:08:14 +0000

    test(types): update ID_PREFIXES count for fronting-report addition

--- packages/types/src/__tests__/ids.test.ts
+++ packages/types/src/__tests__/ids.test.ts
@@ -181,7 +181,7 @@ describe("ID_PREFIXES", () => {
 
   it("has the same number of entries as EntityType members", () => {
     const prefixCount = Object.keys(ID_PREFIXES).length;
-    expect(prefixCount).toBe(47);
+    expect(prefixCount).toBe(48);
   });
 });
 
