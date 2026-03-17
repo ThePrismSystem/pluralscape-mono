d743c2dc422db3493150ad25ee424ad8f58e58a0
ThePrismSystem <10910959+ThePrismSystem@users.noreply.github.com>
2026-03-17 22:23:18 +0000

    chore(db): regenerate migrations

---
+++ packages/db/migrations/pg/0003_quick_red_skull.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "audit_log" DROP CONSTRAINT "audit_log_event_type_check";--> statement-breakpoint
+ALTER TABLE "audit_log" ADD CONSTRAINT "audit_log_event_type_check" CHECK ("audit_log"."event_type" IS NULL OR "audit_log"."event_type" IN ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33, $34, $35, $36, $37, $38, $39, $40, $41, $42, $43));
