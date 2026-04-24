254ac29837995f2b1910c1378f2ce0b8c17b9109
ThePrismSystem <10910959+ThePrismSystem@users.noreply.github.com>
2026-04-24 05:53:42 -0500

    test(types): bump ID_PREFIXES count assertion to 65 (adds smc_)

--- packages/types/src/__tests__/ids.test.ts
+++ packages/types/src/__tests__/ids.test.ts
@@ -213,7 +213,7 @@ describe("ID_PREFIXES", () => {
 
   it("has the same number of entries as EntityType members", () => {
     const prefixCount = Object.keys(ID_PREFIXES).length;
-    expect(prefixCount).toBe(64);
+    expect(prefixCount).toBe(65);
   });
 });
 
